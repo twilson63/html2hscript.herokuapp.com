b13151eed5a97f91a37e2300bbc1131b2463699b
Tom Wilson <tom@jackhq.com>
2015-02-09 20:59:51 -0500

    added jackhq.com

--- app.js
+++ app.js
@@ -28,8 +28,13 @@ function render(state) {
     ]),
     h('.row', [
       h('form', { action: '/parse', method: 'post'}, [
-        h('.twelve.columns', [
+        h('.eight.columns', [
           h('textarea.u-full-width', {name: 'html', placeholder: 'Add HTML Snippet Here'})
+        ]),
+        h('.three.columns', [
+          h('a', {href: 'http://www.jackhq.com'}, [
+            h('img.u-full-width', {src: '//www.jackhq.com/images/jackhq-logo.png'})
+          ])
         ]),
         h('.twelve.columns', [
           h('button.button.button-primary', ['Convert from HTML to HyperScript'])
